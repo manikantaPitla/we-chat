73eb68a6f1e80bb3d7fb598e877b5b8e70ca56a4
maniDynamic <pitlamanikanta8185@gmail.com>
2023-11-13 18:08:20 +0530

    CHAT SERVER

--- index.js
+++ index.js
@@ -14,38 +14,136 @@ const io = require("socket.io")(server, {
   },
 });
 
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+
 io.on("connection", (socket) => {
   console.log(`user connected : ${socket.id}`);
 
-  socket.on("joinRoom", ({ room, name }) => {
-    socket.join(room);
+  socket.on("joinRoom", ({ room, name, avatar }) => {
+    const user = userJoin(socket.id, name, room, avatar);
+    socket.join(user.room);
 
     socket.emit("userConnected", {
       sender: "server",
-      message: `Welcome to the chat ${name}`,
+      message: `Welcome to the chat ${user.name}`,
       time: null,
       avatar: null,
     });
-    socket.broadcast.to(room).emit("userConnected", {
+
+    socket.broadcast.to(user.room).emit("userConnected", {
       sender: "server",
-      message: `${name} joined the chat`,
+      message: `${user.name} joined the chat`,
       time: null,
       avatar: null,
     });
 
     socket.on("messenger", (msg) => {
-      io.to(room).emit("messenger", msg);
+      io.to(user.room).emit("messenger", msg);
+    });
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
     });
   });
 
   socket.on("disconnect", () => {
-    socket.broadcast.emit("userDisconnected", "A user has left the chat");
-    console.log("A user disconnected");
+    const user = userLeave(socket.id);
+    console.log(user);
+    if (user) {
+      io.to(user.room).emit("userDisconnected", {
+        sender: "server",
+        message: `${user.name} left the chat`,
+        time: null,
+        avatar: null,
+      });
+      console.log(`${user.name} user disconnected`);
+
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+    console.log("disconnected");
   });
 });
+// socket.on("disconnect", () => {
+//     const user = userLeave(socket.id);
 
+//     if (user) {
+//       io.to(user.room).emit(
+//         "message",
+//         formatMessage(botName, `${user.username} has left the chat`)
+//       );
+
+//       // Send users and room info
+//       io.to(user.room).emit("roomUsers", {
+//         room: user.room,
+//         users: getRoomUsers(user.room),
+//       });
+//     }
+//   });
 const PORT = 3010;
 
 server.listen(PORT, () => {
   console.log(`Server Running on http://localhost:${PORT}`);
 });
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // Run when client connects
+// io.on("connection", (socket) => {
+//   console.log(io.of("/").adapter);
+//   socket.on("joinRoom", ({ username, room }) => {
+//     const user = userJoin(socket.id, username, room);
+
+//     socket.join(user.room);
+
+//     // Welcome current user
+//     socket.emit("message", formatMessage(botName, "Welcome to ChatCord!"));
+
+//     // Broadcast when a user connects
+//     socket.broadcast
+//       .to(user.room)
+//       .emit(
+//         "message",
+//         formatMessage(botName, `${user.username} has joined the chat`)
+//       );
+
+//     // Send users and room info
+//     io.to(user.room).emit("roomUsers", {
+//       room: user.room,
+//       users: getRoomUsers(user.room),
+//     });
+//   });
+
+//   // Listen for chatMessage
+//   socket.on("chatMessage", (msg) => {
+//     const user = getCurrentUser(socket.id);
+
+//     io.to(user.room).emit("message", formatMessage(user.username, msg));
+//   });
+
+//   // Runs when client disconnects
+//   socket.on("disconnect", () => {
+//     const user = userLeave(socket.id);
+
+//     if (user) {
+//       io.to(user.room).emit(
+//         "message",
+//         formatMessage(botName, `${user.username} has left the chat`)
+//       );
+
+//       // Send users and room info
+//       io.to(user.room).emit("roomUsers", {
+//         room: user.room,
+//         users: getRoomUsers(user.room),
+//       });
+//     }
+//   });
+// });
